d8b0ac2ba4c6354c21fde3566346e48a0bbd4949
moretall <seongho4996@naver.com>
2022-03-10 22:19:38 +0900

    fix : ReactQueryExample

--- src/recoil/RecoilExample.tsx
+++ src/recoil/RecoilExample.tsx
@@ -1,35 +1,46 @@
 // @flow
-import * as React from 'react';
-import {useUser} from "./states/hooks/useUser";
-import {useEffect} from "react";
-import {getUser} from "../api";
+import * as React from "react";
+import { useUser } from "./states/hooks/useUser";
+import { useEffect } from "react";
+import { getUser, lazyApi } from "../api";
+import { Loading } from "../components/atoms/Loading";
 
-type Props = {
-
-};
+type Props = {};
 export const RecoilExample = (props: Props) => {
-    const [user, setUser] = useUser();
+  const [user, setUser] = useUser();
+  const [loading, setLoading] = React.useState(false);
 
-    useEffect(() => {
-        (async () => {
-            const user = await getUser(1)
-            setUser(user);
-        })()
-    }, [setUser])
+  const fetch = React.useCallback(() => {
+    (async () => {
+      setLoading(false);
+      console.log(loading);
+      await lazyApi(5000);
+      setLoading(true);
+      console.log(loading);
+    })();
+  }, []);
 
-    return (
-        <div>
-            <RecoilExampleInner/>
-        </div>
-    );
+  useEffect(() => {
+    (async () => {
+      const user = await getUser(1);
+      setUser(user);
+    })();
+  }, [setUser]);
+
+  return (
+    <div>
+      <button onClick={() => fetch()}></button>
+      <RecoilExampleInner />
+    </div>
+  );
 };
 
 export const RecoilExampleInner = () => {
-    const [user, setUser] = useUser();
-    return (
-        <div>
-            {user?.id}
-            {user?.name}
-        </div>
-    );
-}
+  const [user, setUser] = useUser();
+  return (
+    <div>
+      {user?.id}
+      {user?.name}
+    </div>
+  );
+};
